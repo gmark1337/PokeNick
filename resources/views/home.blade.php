d4343645d0474aee1d360db15fd09b7b5d11778c
gmark1337 <gadosmark7@gmail.com>
2026-02-23 18:58:47 +0100

    install npm packages for UI, and add a simple landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        PokeNick
+    </x-slot:title>
+    <div class="max-w-2xl mx-auto">
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <h1 class="text-3xl font-bold">Welcome to PokeNick!</h1>
+                        <p class="mt-4 text-base-content/60">Simple website for managing your pokemons, 
+                            train them  add various nicknames or let the system do it for you!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</x-layout>
